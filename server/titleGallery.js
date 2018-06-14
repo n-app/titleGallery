bfbb714bf444e7343a4e103e742eff844e8ab7b3
clfhhc <clfhhc@gmail.com>
2018-06-13 17:41:13 -0700

    adding server

---
+++ server/titleGallery.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+const roomIdAdjustment = -1000 + 1;
+
+router.get('/:roomId', (req, res, next) => {
+
+});
+
+router.post('/:roomId', (req, res, next) => {
+
+});
+
+module.exports = router;
